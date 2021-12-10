eb3d280cadfdce02f747218600c63747b77ac1a5
Anton Proshchenko <0954264@gmail.com>
2021-12-10 12:15:31 +0200

    works

---
+++ api/sendMail.js
@@ -0,0 +1,43 @@
+import { createTransport } from "nodemailer";
+import sanitizeHtml from "sanitize-html";
+require("dotenv").config();
+
+function getTransporter() {
+  return createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_ADRESS,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+}
+
+async function sendMail(options) {
+  try {
+    const transport = getTransporter();
+    await transport.sendMail(options);
+    return { success: true };
+  } catch (error) {
+    throw new Error(error?.message);
+  }
+}
+const from = `Paul Vasssssss - ${process.env.EMAIL_ADRESS}`;
+async function formSubmit(formData) {
+  let html = "";
+  for (const option in formData) {
+    html += option + " : " + formData[option] + "<br/>";
+  }
+  return sendMail({
+    from,
+    to: process.env.EMAIL_TO_USER,
+    subject: "New form submision",
+    html: sanitizeHtml(html),
+  });
+}
+
+module.exports = async (req, res) => {
+  const result = await formSubmit(req.body);
+  res.json({ result });
+};
---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+	const formData = {}
+	const submit = async () => {
+		const res = await fetch('/api/sendMail', {
+			method:'POST',
+			headers: {
+					'Content-Type': 'application/json;charset=utf-8'
+				},
+				body: JSON.stringify(formData)
+		})
+		const res1 = await res.json();
+		console.log(res1);
+	}
+</script>
+
+<main>
+	<!-- forma with inputs -->
+	<form on:submit|preventDefault={submit}>
+		<input name="email" type="email" placeholder="Enter your email" bind:value={formData.emai}/>
+		<input name="name" type="text" placeholder="Enter your name" bind:value={formData.name}/>
+		<input name="password" type="password" placeholder="Enter your password" bind:value={formData.password}/>
+		<input name="confirmPassword" type="password" placeholder="Confirm password" bind:value={formData.confirmPassword}/>
+		<input type="submit"/>
+	</form>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+</style>
